3bc4d1ca2c6571772a32b56ec58a1e63a9d0abef
Jerek Dain <jerekdain@gmail.com>
2017-10-20 16:38:07 -0500

    improve the router

--- library/Router.php
+++ library/Router.php
@@ -13,11 +13,32 @@ class Router {
     // ********************** //
 
     /**
+     * The path requested by the user.
+     *
+     * @var array|null
+     */
+    private $path = null;
+
+    /**
+     * The parsed array of any query string on the request, or null.
+     *
+     * @var array|null
+     */
+    private $query = null;
+
+    /**
+     * Any query string on the request, or null.
+     *
+     * @var string|null
+     */
+    private $queryString = null;
+
+    /**
      * The user's request.
      *
      * @var string
      */
-    private $request = null;
+    private $requestString = null;
 
     // ********************* //
     // ***** FUNCTIONS ***** //
@@ -28,17 +49,40 @@ class Router {
     // ------ //
 
     /**
-     * Router constructor.
+     * Parse the request URI and store the results.
      */
     public function __construct() {
-        $this->request = $_SERVER['REQUEST_URI'];
+        // Store the base request
+        $this->requestString = urldecode($_SERVER['REQUEST_URI']);
+
+        // Store the full query string
+        $this->queryString = urldecode($_SERVER['QUERY_STRING']);
+
+        // Prepare a request path string without the leading slash
+        $requestPath = preg_replace('~^/~', '', $this->requestString);
+
+        if (is_string($this->queryString)) {
+            // Parse the query string and store it as an array
+            parse_str($this->queryString, $this->query);
+
+            // Remove the query string from the request
+            $needle = '?' . $this->queryString;
+            if (($pos = strpos($requestPath, $needle)) !== false) {
+                $requestPath = substr($requestPath, 0, $pos);
+            }
+        }
+
+        // Store an array of the request path
+        $this->path = explode('/', $requestPath);
     }
 
     /**
      * Execute on the route.
      */
     public function dispatch() {
-        switch(self::getPage()) {
+        $request = $this->getRequest();
+
+        switch ($request['path'][0]) {
             default:
                 echo \RpgTools::renderView('home');
         }
@@ -49,20 +93,14 @@ class Router {
     // ------- //
 
     /**
-     * Get the page string, so the router knows where we're going.
-     *
-     * @return string
-     */
-    private function getPage() {
-        return self::getRequest()[0];
-    }
-
-    /**
      * Parse and return the request details.
      *
      * @return array
      */
     private function getRequest() {
-        return explode('/', $this->request);
+        return [
+            'path' => $this->path,
+            'query' => $this->query,
+        ];
     }
 }
